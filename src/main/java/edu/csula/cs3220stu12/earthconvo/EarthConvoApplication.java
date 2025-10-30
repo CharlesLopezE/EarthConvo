aefdb80417621b5531caac9dbd0c64cf96a0866a
Llama <150065016+CharlesLopezE@users.noreply.github.com>
2025-10-30 14:04:21 -0700

    Commited EarthConvo project to GitHub.

---
+++ src/main/java/edu/csula/cs3220stu12/earthconvo/EarthConvoApplication.java
@@ -0,0 +1,13 @@
+package edu.csula.cs3220stu12.earthconvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthConvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarthConvoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/csula/cs3220stu12/earthconvo/EarthConvoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.csula.cs3220stu12.earthconvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthConvoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
